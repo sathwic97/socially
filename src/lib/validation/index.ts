bdad5ad648dbfde8755503cc46a52c848a41b181
sathwic97 <sathwic97@gmail.com>
2023-11-13 23:13:46 +0530

    connecting appwrite

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,8 +2,8 @@ import * as z from "zod";
 
 export const SignupValidation = z.object({
     name : z.string().min(2,{message: 'too short'}),
-    username: z.string().min(2).max(50),
-    email : z.string().min(2,{message:'too short'}),
+    username: z.string().min(2,{message: 'too short'}).max(50,{message: 'invalid'}),
+    email : z.string().email({message:'invalid email'}),
     password : z.string().min(8, { message: 'password must be at least 8 characters long' }),
 
 })
